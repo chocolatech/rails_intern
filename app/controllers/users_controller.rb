d82c6be8c09304be28506f121d6aa16a641fb6db
cobbie <evelinagonda@gmail.com>
2014-07-17 16:10:53 +0200

    zmiana priv2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ def show
 @user=current_user
 end
 
-private def make_sure_its_mine
+private 
+def make_sure_its_mine
 unless current_user.id == params[:id].to_i
 redirect_to root_url, alert: "To nie twoje konto"
 end
